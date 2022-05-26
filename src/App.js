396095d26c6705f462d49ad1647769c9ea44e15c
Farhad <alliance_avto.abd@inbox.ru>
2022-05-26 11:29:28 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from "react";
 import {BrowserRouter, Routes, Route,} from "react-router-dom";
 import './App.css';
-
-
+import Header from "./Header";
+import About from "./About";
+import Price from "./Price";
+import Information from "./Information";
+import People from "./People";
 import Todo from "./Todo";
-
+import Todo2 from "./Todo2";
+import Todo3 from "./Todo3";
 
 
 function App() {
   return (
    <BrowserRouter>
+       <Header />
       <div className="container">
           <Routes>
-               <Route path="/Todo" element={<Todo />}/>
+              <Route path="/About" element={<About />}/>
+              <Route path="/Price" element={<Price />}/>
+              <Route path="/Information" element={<Information />}/>
+              <Route path="/People" element={<People />}/>
+              <Route path="/Todo" element={<Todo />}/>
+              <Route path="/Todo2" element={<Todo2 />}/>
+              <Route path="/Todo3" element={<Todo3 />}/>
+
           </Routes>
       </div>
    </BrowserRouter>
